54b871330040e406f3d7cc94589bf373e6daa645
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2022-10-30 15:07:13 -0700

    Selection Sort Added

--- AlgoTest/algorithms.js
+++ AlgoTest/algorithms.js
@@ -12,6 +12,22 @@ const algorithms = {
             }
         }
         return array
+    },
+    SelectionSort: function(array) {
+        for(let i = 0; i < array.length; ++i){
+            let smallestIndex = i
+            let smallest = array[i]
+            for(let j = i + 1; j < array.length; ++j){
+                if(array[j] < smallest) {
+                    smallestIndex = j
+                    smallest = array[j]
+                }
+            }
+            //Swap
+            array[smallestIndex] = array[i]
+            array[i] = smallest
+        }
+        return array
     }
 }
 
