68382d06bd3f76b8686765e6fd63ff94b21ae7de
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2022-10-31 22:41:11 -0700

    Insertion Sort v3 Added

--- InsertionSort/v3/index.js
+++ InsertionSort/v3/index.js
@@ -19,12 +19,11 @@ class svgValue {
         .range([0, svg.attr('width')])
         .paddingInner(0.12)
 }
-
+const numOfElements = 10;
 const maxVal = 50
-//Array of svgValue
-let rectObjArray = []
-//Array of order 
-const steps = []
+let rectObjArray = [] // Array of Rectangle SVG
+const rectSteps = [] // Array of Rect Order
+const colorSteps = [] // Array of Color Order
 const svg = d3.select('#svg')
 /*
     Generate Array of Random Numbers
@@ -32,31 +31,34 @@ const svg = d3.select('#svg')
 */
 reset()
 function reset() {
-    //Initialize the Array of Rectangle Object Values
+    //Remove Rect SVG Elements on the DOM
     if(rectObjArray.length > 0) {
-        for(let i = 0; i < 10; ++i){
-            rectObjArray[i].svgRect.remove()
+        for(let i = 0; i < numOfElements; ++i){
+            rectObjArray[i].remove()
         }
     }
     rectObjArray = []
-    for(let i = 0; i < 10; ++i) {
-        const svgValueIndex = new svgValue(i, Math.floor(Math.random() * maxVal) + 1, svg)
+    for(let i = 0; i < numOfElements; ++i) {
+        const svgValueIndex = svg.append('rect')
+            .attr('')
+
+        
+        svgValue(i, Math.floor(Math.random() * maxVal) + 1, svg)
         rectObjArray.push(svgValueIndex)
     }
 
     //Sorting Algorithm
-    for(let i = 1; i < rectObjArray.length; ++i) {
-        //Var to keep arr[i] on memory
-        const valueIndex = rectObjectArray[i].value
-        //Index J for the next loop
-        var j = i - 1
+    // for(let i = 1; i < rectObjArray.length; ++i) {
+    //     //Var to keep arr[i] on memory
+    //     const valueIndex = rectObjectArray[i].value
+    //     //Index J for the next loop
+    //     var j = i - 1
     
-        while(j >= 0 && rectObjArray[j].value > valueIndex) {
-            arr[j + 1] = arr[j]
-            --j;
-            arr[j + 1] = valueIndex //Because of j-- on the while loop, we need to index at j + 1
+    //     while(j >= 0 && rectObjArray[j].value > valueIndex) {
+    //         arr[j + 1] = arr[j]
+    //         --j;
+    //         arr[j + 1] = valueIndex //Because of j-- on the while loop, we need to index at j + 1
             
-        }
-    }
+    //     }
+    // }
 }
-console.log(rectObjArray)
