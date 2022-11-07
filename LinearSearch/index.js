f48d9a3a93dcc292e73f0d42657ac041d57c7e1c
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2022-11-06 21:47:40 -0800

    rect and step class added

--- LinearSearch/index.js
+++ LinearSearch/index.js
@@ -1,6 +1,41 @@
-const Render = require('./Classes/Render.js')
+class RectObj {
+    constructor(val) {
+        this.val = val
+    }
+}
 
+class StepObj{
+    constructor(stepNumber, rectOrder, ColorOrder) {
+        this.stepNumber = stepNumber
+    }
+    
+    renderStep() {
+        if(this.color == -1) {
+            for(let i = 0; i < rectOrder.length; ++i) {
+                rectOrder[i].renderRect(i)
+            }
+        }
+        else {
+            for(let i = 0; i < rectOrder.length; ++i) {
+                rectOrder[i].renderRect(i)
+                rectOrder[i].renderColor(colorOrder[i])
+            }
+        }
+    }
+}
+
+/*
+    Return an array of svg:rect
+*/
+function prepareRectSvg(svg, size) {
+    const rectArray = []
+    for(let i = 0 ;  i < size; ++i) {
+        const rectIndex = svg.append('rect')
+            .attr('class', 'rectSvg')
+    }
+    return rectArray
+}
 const svg = d3.select('#svg')
-const render = new Render(svg)
-
-render.renderRectArray(10)
+function reset() {
+    const RectObjArray = prepareRectSvg(svg, 10)
+}
