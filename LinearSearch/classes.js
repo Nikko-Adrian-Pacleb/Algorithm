2212caef98db5a323057ea128d68c5f50acc4b7f
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2022-11-10 23:39:29 -0800

    .

--- LinearSearch/classes.js
+++
@@ -1,8 +0,0 @@
-class RectObj {
-    constructor(svg, val) {
-        this.val = val
-        this.rectSVG = svg.append('rect')
-            .attr('width', 100)
-            .attr('height', 100)
-    }
-}
