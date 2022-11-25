5c8fd3c799683b73aa261321c2b62111b12fa559
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2022-11-24 20:14:41 -0800

    Update Colors and folders

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// const Selection = {
+//     "selection" : [
+//         {"name": "Search","type":"dropdown", "items": [
+//             "Linear Search",
+//             "Binary Search"
+//         ]},
+//         {"name": "Sort", "type":"dropdown", "items": [
+//             "Bubble Sort",
+//             "Insertion Sort",
+//             "Selection Sort",
+//             "Merge Sort"
+//         ]}
+//     ]
+// }
+
 const Selection = {
-    "selection" : [
-        {"name": "Search","type":"dropdown", "items": [
-            "Linear Search",
-            "Binary Search"
-        ]},
-        {"name": "Sort", "type":"dropdown", "items": [
-            "Bubble Sort",
-            "Insertion Sort",
-            "Selection Sort",
-            "Merge Sort"
-        ]}
+    "selection": [
+        {"name": "Search", "website": "./Search"},
+        {"name": "Sort", "website": "./Sort"}
     ]
 }
+
 function selectionSetup() {
     const SelectionDOM = document.getElementById("selection")
     for(let i in Selection.selection){
@@ -20,6 +28,9 @@ function selectionSetup() {
         SelectionIndexDOM.textContent = Selection.selection[i].name
         SelectionIndexDOM.id = `selection-choice-${Selection.selection[i].name}`
         SelectionIndexDOM.className = 'selection-dropdown'
+        SelectionIndexDOM.addEventListener('click', function() {
+            
+        })
         SelectionDOM.appendChild(SelectionIndexDOM)
     }
 }
