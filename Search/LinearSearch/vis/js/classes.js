8755a7c82c1c9542b608437624a7d6cd50f6c949
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-01-31 18:02:30 -0800

    Linear Search Folder Fix

--- Search/LinearSearch/vis/js/classes.js
+++ Search/LinearSearch/vis/js/classes.js
@@ -1,75 +1,144 @@
-const LinearClassSVG = d3.select('#vis-svg-linear')
-class RectVis {
-    //Constants
-    maxVal = 50
-    numOfElements = 10
-    
-    //Colors
-    colorN = '#F2E205'
-    colorI = '#F2A71B'
-    colorD = '#277FF2'
+// Constants
+const svg = d3.select('#vis-svg-linear')
+const maxVal = 50
+const minVal = 20
 
-    //Arrays
-    stepIndex = 0
-    rectObjArray = []
-    
-    //Button Array
-    buttonArray = []
+class Render{
+    colorNeutral = '#F2E205'
+    colorIndex = '#F2A71B'
+    colorFound = '#277FF2'
 
-    //Constructor
-    constructor(id) {
-        this.id = id
+    ValueToFind = 25
+    
+    StepNumber = 0
+    RectangleArray = []
+    StepsColor = []
+    
+    constructor() {
+        
     }
 
     //Remove RectSVG Elements on the DOM and Clear RectObjArray
     ClearSVG() {
         //Remove RectSVG on DOM
-        if(this.rectObjArray.length > 0) {
-            for(let i = 0; i < this.numOfElements; ++i) {
-                this.rectObjArray[i].remove()
+        if(this.RectangleArray.length > 0) {
+            for(let i = 0; i < 10; ++i) {
+                this.RectangleArray[i].GroupSvg.remove()
             }
         }
 
-        this.rectObjArray = [] //Clear Array
+        this.RectangleArray = [] //Clear Array
     }
     
     //Create RectObjs and save it to array
-    CreateRect() {
-        for(let i = 0; i < this.numOfElements; ++i) {
-            this.rectObjArray.push(new RectObj(i, Math.floor(Math.random() * this.maxVal) + 1, this.maxVal))
+    CreateRectangles() {
+        for(let i = 0; i < 10; ++i) {
+            this.RectangleArray.push(new Rectangle(i, Math.floor(Math.random() * (maxVal - minVal)) + minVal))
         }
     }
 
-    SetupRect() {
+    //Simple run of 2 functions to properly setup the svg and the rectangles
+    SetupRectangles() {
         this.ClearSVG()
-        this.CreateRect()
+        this.CreateRectangles()
+        this.StepsColor = []
+        this.GetStepsColor()
+        this.StepNumber = 0
+        this.RenderStepsColor(this.StepNumber)
     }
 
-    CreateButton(buttonId, buttonText, buttonFunction, xPos, yPos) {
-        const newButton = new SVGButton(this.id, buttonId, buttonText, buttonFunction, xPos, yPos)
-        this.buttonArray.push(newButton)
+    // Change the Value that the search algorithm is looking for
+    SetValueToFind(value) {
+        this.ValueToFind = value
+        this.StepNumber = 0
+        this.StepsColor = []
+        this.GetStepsColor()
+        console.log(this.StepsColor)
+        this.RenderStepsColor(0)
+    }
+
+    // Generate the steps for colors and rect positions
+    GetStepsColor() {
+        let found = false
+        // Color Initialization
+        const tempStepsColor = []
+        for(let i = 0; i < 10; ++i) {
+            tempStepsColor.push(this.colorNeutral)
+        }
+        this.StepsColor.push([...tempStepsColor])
+
+        for(let i = 0; i < 10; ++i) {
+            if(this.RectangleArray[i].value == this.ValueToFind) {
+                found = true
+                tempStepsColor[i] = this.colorFound
+                this.StepsColor.push([...tempStepsColor])
+            }
+            else {
+                tempStepsColor[i] = this.colorIndex
+                this.StepsColor.push([...tempStepsColor])
+            }
+        }
+
+        return found
+    }
+
+    //Increate Step Number and Render the next step
+    GetNextStep() {
+        if(this.StepNumber == this.StepsColor.length - 1){
+            return
+        }
+        
+        this.StepNumber++
+
+        this.RenderStepsColor(this.StepNumber)
+    }
+
+    //Increate Step Number and Render the previous step
+    GetPreviousStep() {
+        if(this.StepNumber == 0) {
+            return
+        }
+
+        this.StepNumber--
+
+        this.RenderStepsColor(this.StepNumber)
+    }
+
+    RenderStepsColor(index) {
+        const ColorSet = [...this.StepsColor[index]]
+        for(let i = 0; i < 10; ++i) {
+            this.RectangleArray[i].RenderColor(ColorSet[i])
+        }
     }
 }
-class RectObj{
-    constructor(key, value, maxVal) {
 
+class Rectangle{
+    constructor(key, value) {
         this.key = key
         this.value = value
-        this.maxVal = maxVal
-        this.rectSvg = LinearClassSVG.append('rect')
+        this.GroupSvg = svg.append('g')
+        // Rectangle SVG
+        this.RectangleSvg = this.GroupSvg.append('rect')
             .attr('id', `rect-object-${key}`)
             .attr('class', 'rect-object')
             .attr('height', this.yScale(value))
             .attr('width', this.xScale.bandwidth())
+            .attr('x', this.xScale(this.key))
+        // Text SVG
+        this.TextSvg = this.GroupSvg.append('text')
+            .attr('y', 20)
+            .attr('x', this.xScale(this.key) + 5)
+            .text(`${this.value}`)
+            .style('fill', 'white')
     }
     
     yScale = d3.scaleLinear()
-        .domain([0, 50]) //maxVal
-        .range([0, LinearClassSVG.attr('height')]) //LinearClassSVG Height
+        .domain([0, maxVal])
+        .range([0, svg.attr('height')])
 
     xScale = d3.scaleBand()
         .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        .range([0, LinearClassSVG.attr('width')]) //LinearClassSVG Width
+        .range([0, svg.attr('width')])
         .paddingInner(0.12)
 
 
@@ -78,57 +147,9 @@ class RectObj{
     //         .attr('x', this.xScale(xPos))
     // }
 
-    // renderColor(color) {
-    //     this.rectSvg
-    //         .attr('fill', color)
-    // }
-}
-class Steps {
-    constructor(size) {
-        this.size = size
-        this.stepIndex = 0
-        
-    }
-}
-class StepObj{
-    constructor(stepNumber, rectOrder, colorOrder) {
-        this.stepNumber = stepNumber
-        this.rectOrder = rectOrder
-        this.colorOrder = colorOrder
-    }
-    
-    renderStep() {
-        if(this.color == -1) {
-            for(let i = 0; i < rectOrder.length; ++i) {
-                rectOrder[i].renderRect(i)
-            }
-        }
-        else {
-            for(let i = 0; i < rectOrder.length; ++i) {
-                rectOrder[i].renderRect(i)
-                rectOrder[i].renderColor(colorOrder[i])
-            }
-        }
+    RenderColor(color) {
+        this.RectangleSvg
+            .attr('fill', color)
     }
 }
 
-class SVGButton {
-    constructor(visId, buttonId,buttonText, xPos, yPos, buttonFunction) {
-        this.buttonText = buttonText
-        this.buttonFunction = buttonFunction
-        this.buttonGroup = LinearClassSVG.append('g')
-            .attr('class', 'svg-button')
-            .attr('id', `svg-button-${visId}-${buttonId}`)
-        this.buttonGroup.append("rect")
-            .attr('class', 'button-background')
-            .attr('x', xPos)
-            .attr('y', yPos)    
-            .attr('width', 100)
-            .attr('height', 57)
-        this.buttonGroup.append('text')
-            .attr('class', 'button-text')
-            .attr('x', xPos)
-            .attr('y', yPos)
-            .text(buttonText)
-    }
-}
--- Search/LinearSearch/vis/js/index.js
+++ Search/LinearSearch/vis/js/index.js
@@ -1,12 +1,19 @@
-// SVG Setup
-const LinearVis = new RectVis("linear")
-function reset() {
-    LinearVis.ClearSVG()
-    LinearVis.CreateRect()
-    LinearVis.CreateButton("TrialID", "TrialText", 200, 200, function() {
-        console.log("Button Created")
-    })
-    // Vis.SetupRect()
+function LinearReset() {
+    LinearRender.SetupRectangles()
 }
 
-reset()
+function LinearNext() {
+    LinearRender.GetNextStep()
+}
+
+function LinearPrevious() {
+    LinearRender.GetPreviousStep()
+}
+
+function LinearUpdate() {
+    const value = parseInt(document.getElementById("linear-vis-extra-input-find").value)
+    LinearRender.SetValueToFind(value)
+}
+const LinearRender = new Render()
+
+LinearRender.SetupRectangles()
