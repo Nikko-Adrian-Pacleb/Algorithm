2a48bb0cf415e0a14621eb1cc62176762d544b23
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-02-08 23:22:04 -0800

    Visual Updates

--- scripts/index.js
+++ scripts/index.js
@@ -21,18 +21,31 @@ const Selection = {
 }
 
 function selectionSetup() {
-    const SelectionDOM = document.getElementById("selection")
-    for(let i in Selection.selection){
+    // const SelectionDOM = document.getElementById("selection")
+    // for(let i in Selection.selection){
+    //     console.log(Selection.selection[i])
+    //     const SelectionIndexDOM = document.createElement('button')
+    //     SelectionIndexDOM.textContent = Selection.selection[i].name
+    //     SelectionIndexDOM.id = `selection-choice-${Selection.selection[i].name}`
+    //     SelectionIndexDOM.className = 'selection-dropdown'
+    //     SelectionIndexDOM.addEventListener('click', function() {
+    //         window.open(`${Selection.selection[i].website}`, "_self") //"_self" opens the site in the same tab
+    //         // window.open(`${Selection.selection[i].website}`, "_self")
+    //     })
+    //     SelectionDOM.appendChild(SelectionIndexDOM)
+    // }
+
+    const SelectionDiv = document.getElementById('selection')
+    for(let i in Selection.selection) {
         console.log(Selection.selection[i])
-        const SelectionIndexDOM = document.createElement('button')
+        const SelectionIndexDOM = document.createElement('div')
+        SelectionIndexDOM.id=`selection-div-${Selection.selection[i].name}`
+        SelectionIndexDOM.className = 'selection-choice'
         SelectionIndexDOM.textContent = Selection.selection[i].name
-        SelectionIndexDOM.id = `selection-choice-${Selection.selection[i].name}`
-        SelectionIndexDOM.className = 'selection-dropdown'
         SelectionIndexDOM.addEventListener('click', function() {
-            window.open(`${Selection.selection[i].website}`, "_self") //"_self" opens the site in the same tab
-            // window.open(`${Selection.selection[i].website}`, "_self")
+            window.open(`${Selection.selection[i].website}`, "_self")
         })
-        SelectionDOM.appendChild(SelectionIndexDOM)
+        SelectionDiv.appendChild(SelectionIndexDOM)
     }
 }
 
