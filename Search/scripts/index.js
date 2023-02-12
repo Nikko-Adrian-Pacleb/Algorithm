aa64d566e9bed2f87173efdea960bb2e9d8d5522
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-02-11 20:31:02 -0800

    Visual Update

---
+++ Search/scripts/index.js
@@ -0,0 +1,8 @@
+//Creating a SVGDOM Element Yay
+// const visContainers = document.getElementById('trial-svg-div')
+// var svgDOM = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+// svgDOM.setAttribute('width', '1200px')
+// svgDOM.setAttribute('height', '510px')
+
+// visContainers.appendChild(svgDOM)
+// document.body.appendChild(svgDOM)
