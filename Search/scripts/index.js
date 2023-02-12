308f00ff19922007636fb670053c2f68a6e92044
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-02-11 22:35:07 -0800

    DOM Update

--- Search/scripts/index.js
+++ Search/scripts/index.js
@@ -9,22 +9,22 @@
 // visContainers.appendChild(svgDOM)
 // document.body.appendChild(svgDOM)
 
-
+/* Add to a branch or something */
 // Window Size Update
-let windowHeight = 0
-let windowWidth = 0
-function updateSize() {
-    windowHeight = window.innerHeight
-    windowWidth = window.innerWidth
-}
-updateSize()
-window.addEventListener('resize', updateSize)
+// let windowHeight = 0
+// let windowWidth = 0
+// function updateSize() {
+//     windowHeight = window.innerHeight
+//     windowWidth = window.innerWidth
+// }
+// updateSize()
+// window.addEventListener('resize', updateSize)
 
-// Add SVG Elements
-const visContainers = document.getElementsByClassName('vis-container')
-for(let i = 0; i < visContainers.length; ++i) {
-    const svgDOM = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svgDOM.setAttribute('width', (windowWidth - 200))
-    svgDOM.setAttribute('height', '510')
-    visContainers[i].appendChild(svgDOM)
-}
+// // Add SVG Elements
+// const visContainers = document.getElementsByClassName('vis-container')
+// for(let i = 0; i < visContainers.length; ++i) {
+//     const svgDOM = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+//     svgDOM.setAttribute('width', (windowWidth - 200))
+//     svgDOM.setAttribute('height', '510')
+//     visContainers[i].appendChild(svgDOM)
+// }
