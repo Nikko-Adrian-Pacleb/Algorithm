a21f2073e8b3c47f8877ab73cea4b5b7eeb5b455
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-02-11 20:47:31 -0800

    Add Trial JS SVG Append

--- Search/scripts/index.js
+++ Search/scripts/index.js
@@ -3,6 +3,28 @@
 // var svgDOM = document.createElementNS("http://www.w3.org/2000/svg", "svg")
 // svgDOM.setAttribute('width', '1200px')
 // svgDOM.setAttribute('height', '510px')
-
+/*
+    Make sure to not place the script on the head because it will be read first than the html
+*/
 // visContainers.appendChild(svgDOM)
 // document.body.appendChild(svgDOM)
+
+
+// Window Size Update
+let windowHeight = 0
+let windowWidth = 0
+function updateSize() {
+    windowHeight = window.innerHeight
+    windowWidth = window.innerWidth
+}
+updateSize()
+window.addEventListener('resize', updateSize)
+
+// Add SVG Elements
+const visContainers = document.getElementsByClassName('vis-container')
+for(let i = 0; i < visContainers.length; ++i) {
+    const svgDOM = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    svgDOM.setAttribute('width', (windowWidth - 200))
+    svgDOM.setAttribute('height', '510')
+    visContainers[i].appendChild(svgDOM)
+}
