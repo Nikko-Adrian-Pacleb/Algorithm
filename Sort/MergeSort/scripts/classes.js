7fc29a85788d0697cfaf28859bfac12ba848c6a3
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-03-13 03:41:07 -0700

    Content Update

--- Sort/MergeSort/scripts/classes.js
+++ Sort/MergeSort/scripts/classes.js
@@ -30,7 +30,7 @@ class RenderMerge{
     // rectObjArray = [] // Array of Rectangle SVG
     // stepsObjArray = [] //Array of Steps Object
     StepNumber = 0
-    SquareleArray = []
+    SquareArray = []
     ColorOrderArray = []
     SquareOrderArray = []
 
@@ -41,13 +41,13 @@ class RenderMerge{
     //Remove RectSVG Elements on the DOM and Clear RectObjArray
     ClearSVG() {
         //Remove RectSVG on DOM
-        if(this.RectangleArray.length > 0) {
+        if(this.SquareArray.length > 0) {
             for(let i = 0; i < 10; ++i) {
-                this.RectangleArray[i].GroupSvg.remove()
+                this.SquareArray[i].GroupSvg.remove()
             }
         }
 
-        this.RectangleArray = [] //Clear Array
+        this.SquareArray = [] //Clear Array
     }
     
     //Create RectObjs and save it to array
@@ -55,10 +55,18 @@ class RenderMerge{
         const cursorX = (svgWidth / 2) - ((squareWidth * 4) + 7)
         const cursorY = 40 //40 is the margin from the top
         for(let i = 0; i < this.numOfElements; ++i) {
-            this.SquareleArray.push(new MergeRectangle(Math.floor(Math.random() * (maxValMerge - minValMerge)) + minValMerge), cursorX, cursorY)
+            this.SquareArray.push(new MergeRectangle(Math.floor(Math.random() * (maxValMerge - minValMerge)) + minValMerge, cursorX, cursorY))
         }
-        // console.log(this.RectangleArray)
-        this.MergeSort(this.RectangleArray)
+        console.log(this.SquareArray)
+        const SquareArrayCopy = [...this.SquareArray]
+        this.MergeSort(SquareArrayCopy)
+        console.log(SquareArrayCopy)
+
+
+        // const randomArray = [20, 29, 12, 43, 15, 23, 76, 28]
+        // const randomArrayCopy = [...randomArray]
+        // this.MergeSort(randomArrayCopy)
+        // console.log(randomArrayCopy)
     }
 
     //Simple run of 2 functions to properly setup the svg and the rectangles
@@ -69,89 +77,139 @@ class RenderMerge{
         this.ColorOrderArray = []
         this.GetStepsColor()
         this.StepNumber = 0
-        console.log(this.RectOrderArray)
-        console.log(this.ColorOrderArray)
+        // console.log(this.RectOrderArray)
+        // console.log(this.ColorOrderArray)
         this.RenderRectangleColorOrder(this.StepNumber)
         // this.RenderColorOrder(this.StepNumber)
         // this.RenderRectangleOrder(this.StepNumber)
     }
 
+    // Merge(array, p, q, r) {
+    //     const mergedArray = []
+    //     let i = 0; //i indexes the smallest remaining element in L[]
+    //     let j = 0; //j indexes the smallest remaining element in R[]
+    //     // console.log(k)
+    //     //While for comparing
+    //     while(i < leftArray.length && j < rightArray.length){
+    //         if(leftArray[i] < rightArray[j]){
+    //             mergedArray.push(leftArray[i])
+    //             array[k] = leftArray[i]
+    //             ++i
+    //         }
+    //         else {
+    //             mergedArray = rightArray[j]
+    //             ++j
+    //         }
+    //     }
+    
+    //     //While for remainders
+    //     while(i < leftArray.length){
+    //         mergedArray.push(leftArray[i])
+    //         ++i
+    //     }
+    //     while(j < rightArray.length){
+    //         mergedArray.push(rightArray[i])
+    //         ++j
+    //     }
+    // }
+    
+    // MergeSortRecursion(array, p, r, indexCursorX, indexCursorY, direction) {
+    //     const thisRecursionNumberOfElements = r - p + 1
+    //     let thisRecursionStartingPoint
+    //     let thisRecursionCursorX = indexCursorX
+    //     let thisRecursionSquareArray = []
+        
+    //     //Visuals
+    //     if(direction == 'left') {
+    //         thisRecursionCursorX -= marginX + (thisRecursionNumberOfElements * squareWidth) + thisRecursionNumberOfElements
+    //     }
+    //     else {
+    //         thisRecursionCursorX += marginX
+    //     }
+    //     thisRecursionStartingPoint = thisRecursionCursorX 
+    //     for(let squareNumber = p; squareNumber <= r; ++squareNumber) {
+    //         const newSquare = new MergeRectangle(array[squareNumber], thisRecursionCursorX, indexCursorY)
+    //         thisRecursionCursorX += squareWidth + 1
+
+    //         thisRecursionSquareArray.push(newSquare)
+    //     }
+        
+    //     if(p >= r) {
+    //         return
+    //     }
+        
+        
+    //     const q = Math.floor((p + r) / 2)
+    //     const thisRecursionMidPointX = (thisRecursionStartingPoint + thisRecursionCursorX) / 2
+        
+    //     const leftRecurssionArray = this.MergeSortRecursion(array, p, q, thisRecursionMidPointX , indexCursorY + marginY, 'left')
+    //     console.log(leftRecurssionArray)
+
+    //     const rightRecursionArray = this.MergeSortRecursion(array, q + 1, r, thisRecursionMidPointX, indexCursorY + marginY, 'right')
+    //     console.log(rightRecursionArray)
+        
+    //     // Delete Square Arrays Before Merge
+    //     this.Merge(thisRecursionSquareArray, leftRecurssionArray, rightRecursionArray, p, q, r)
+
+    //     // return thisRecursionSquareArray
+    // }
+    
     Merge(array, p, q, r) {
-        const mergedArray = []
+        const leftArray = array.slice(p, q + 1)
+        const rightArray = array.slice(q + 1, r + 1)
         let i = 0; //i indexes the smallest remaining element in L[]
         let j = 0; //j indexes the smallest remaining element in R[]
+        let k = p; //k indexes the location in A[]
         // console.log(k)
         //While for comparing
         while(i < leftArray.length && j < rightArray.length){
-            if(leftArray[i] < rightArray[j]){
-                mergedArray.push(leftArray[i])
+            if(leftArray[i].value < rightArray[j].value){
                 array[k] = leftArray[i]
                 ++i
             }
             else {
-                mergedArray = rightArray[j]
+                array[k] = rightArray[j]
                 ++j
             }
+            ++k
         }
     
         //While for remainders
         while(i < leftArray.length){
-            mergedArray.push(leftArray[i])
+            array[k] = leftArray[i]
             ++i
+            ++k
         }
         while(j < rightArray.length){
-            mergedArray.push(rightArray[i])
+            array[k] = rightArray[j]
             ++j
+            ++k
         }
     }
     
-    MergeSortRecursion(array, p, r, indexCursorX, indexCursorY, direction) {
-        const thisRecursionNumberOfElements = r - p + 1
-        let thisRecursionStartingPoint
-        let thisRecursionCursorX = indexCursorX
-        let thisRecursionSquareArray = []
-        
-        //Visuals
-        if(direction == 'left') {
-            thisRecursionCursorX -= marginX + (thisRecursionNumberOfElements * squareWidth) + thisRecursionNumberOfElements
-        }
-        else {
-            thisRecursionCursorX += marginX
-        }
-        thisRecursionStartingPoint = thisRecursionCursorX 
-        for(let squareNumber = p; squareNumber <= r; ++squareNumber) {
-            const newSquare = new MergeRectangle(array[squareNumber], thisRecursionCursorX, indexCursorY)
-            thisRecursionCursorX += squareWidth + 1
-
-            thisRecursionSquareArray.push(newSquare)
-        }
-        
+    MergeSortRecursion(array, p, r) {
         if(p >= r) {
             return
         }
-        
-        
         const q = Math.floor((p + r) / 2)
-        const thisRecursionMidPointX = (thisRecursionStartingPoint + thisRecursionCursorX) / 2
-        
-        const leftRecurssionArray = this.MergeSortRecursion(array, p, q, thisRecursionMidPointX , indexCursorY + marginY, 'left')
-        console.log(leftRecurssionArray)
-
-        const rightRecursionArray = this.MergeSortRecursion(array, q + 1, r, thisRecursionMidPointX, indexCursorY + marginY, 'right')
-        console.log(rightRecursionArray)
-        
-        // Delete Square Arrays Before Merge
-        this.Merge(thisRecursionSquareArray, leftRecurssionArray, rightRecursionArray, p, q, r)
-
-        // return thisRecursionSquareArray
+        this.MergeSortRecursion(array, p, q)
+        this.MergeSortRecursion(array, q + 1, r)
+    
+        this.Merge(array, p, q, r)
     }
     
     MergeSort(array) {
-        const cursorX = (svgWidth / 2) - ((squareWidth * 4) + 7)
-        const cursorY = 40 //40 is the margin from the top
-        this.MergeSortRecursion(array, 0, array.length - 1, cursorX, cursorY)
+        this.MergeSortRecursion(array, 0, array.length - 1)
         return array
     }
+
+
+    // MergeSort(array) {
+    //     const cursorX = (svgWidth / 2) - ((squareWidth * 4) + 7)
+    //     const cursorY = 40 //40 is the margin from the top
+    //     this.MergeSortRecursion(array, 0, array.length - 1, cursorX, cursorY)
+    //     return array
+    // }
 
     // Generate the steps for colors and rect positions
     GetStepsColor() {
@@ -190,24 +248,26 @@ class RenderMerge{
 
         for(let i = 0; i < 10; ++i) {
             if(ColorOrder[0] == -1){
-                this.RectangleArray[RectOrder[i]].RenderRect(i)
+                this.SquareArray[RectOrder[i]].RenderRect(i)
             }
             else {
-                this.RectangleArray[RectOrder[i]].RenderColor(ColorOrder[i])
-                this.RectangleArray[RectOrder[i]].RenderRect(i)
+                this.SquareArray[RectOrder[i]].RenderColor(ColorOrder[i])
+                this.SquareArray[RectOrder[i]].RenderRect(i)
             }
         }
     }
 }
 
 class MergeRectangle{
-    constructor(value) {
+    constructor(value, mrcursorX, mrcursorY) {
         this.value = value
         this.mrcursorX = mrcursorX
         this.mrcursorY = mrcursorY
         this.GroupSvg = svgMerge.append('g')
 
         this.SquareSvg = this.GroupSvg.append('rect')
+            .attr('x', mrcursorX)
+            .attr('y', mrcursorY)
             .attr('width', squareWidth)
             .attr('height', squareWidth)
         this.SquareText = this.GroupSvg.append('text')
