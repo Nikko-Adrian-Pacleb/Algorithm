5550d5ea460ee62035ff4385d73c8b5d2b3c584d
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-03-23 18:41:58 -0700

    Logo Fixed for main page

--- scripts/index.js
+++ scripts/index.js
@@ -14,45 +14,45 @@
 // }
 
 const Selection = {
-    "selection": [
-        {"name": "Search", "website": "./Search/index.html"},
-        {"name": "Sort(soon)", "website": "./Sort/index.html"}
-    ]
-}
+  selection: [
+    { name: "Search", website: "./Search/index.html" },
+    { name: "Sort", website: "./Sort/index.html" },
+  ],
+};
 
 function selectionSetup() {
-    // const SelectionDOM = document.getElementById("selection")
-    // for(let i in Selection.selection){
-    //     console.log(Selection.selection[i])
-    //     const SelectionIndexDOM = document.createElement('button')
-    //     SelectionIndexDOM.textContent = Selection.selection[i].name
-    //     SelectionIndexDOM.id = `selection-choice-${Selection.selection[i].name}`
-    //     SelectionIndexDOM.className = 'selection-dropdown'
-    //     SelectionIndexDOM.addEventListener('click', function() {
-    //         window.open(`${Selection.selection[i].website}`, "_self") //"_self" opens the site in the same tab
-    //         // window.open(`${Selection.selection[i].website}`, "_self")
-    //     })
-    //     SelectionDOM.appendChild(SelectionIndexDOM)
-    // }
+  // const SelectionDOM = document.getElementById("selection")
+  // for(let i in Selection.selection){
+  //     console.log(Selection.selection[i])
+  //     const SelectionIndexDOM = document.createElement('button')
+  //     SelectionIndexDOM.textContent = Selection.selection[i].name
+  //     SelectionIndexDOM.id = `selection-choice-${Selection.selection[i].name}`
+  //     SelectionIndexDOM.className = 'selection-dropdown'
+  //     SelectionIndexDOM.addEventListener('click', function() {
+  //         window.open(`${Selection.selection[i].website}`, "_self") //"_self" opens the site in the same tab
+  //         // window.open(`${Selection.selection[i].website}`, "_self")
+  //     })
+  //     SelectionDOM.appendChild(SelectionIndexDOM)
+  // }
 
-    const SelectionDiv = document.getElementById('selection')
-    for(let i in Selection.selection) {
-        console.log(Selection.selection[i])
-        const SelectionIndexDOM = document.createElement('div')
-        SelectionIndexDOM.id=`selection-div-${Selection.selection[i].name}`
-        SelectionIndexDOM.className = 'selection-choice'
-        SelectionIndexDOM.addEventListener('click', function() {
-            window.open(`${Selection.selection[i].website}`, "_self")
-        })
+  const SelectionDiv = document.getElementById("selection");
+  for (let i in Selection.selection) {
+    console.log(Selection.selection[i]);
+    const SelectionIndexDOM = document.createElement("div");
+    SelectionIndexDOM.id = `selection-div-${Selection.selection[i].name}`;
+    SelectionIndexDOM.className = "selection-choice";
+    SelectionIndexDOM.addEventListener("click", function () {
+      window.open(`${Selection.selection[i].website}`, "_self");
+    });
 
-        const SelectionTextDOM = document.createElement('p')
-        SelectionTextDOM.id = `selection-text-${Selection.selection[i].name}`
-        SelectionTextDOM.className = 'selection-choice-text'
-        SelectionTextDOM.textContent = Selection.selection[i].name
-        SelectionIndexDOM.appendChild(SelectionTextDOM)
+    const SelectionTextDOM = document.createElement("p");
+    SelectionTextDOM.id = `selection-text-${Selection.selection[i].name}`;
+    SelectionTextDOM.className = "selection-choice-text";
+    SelectionTextDOM.textContent = Selection.selection[i].name;
+    SelectionIndexDOM.appendChild(SelectionTextDOM);
 
-        SelectionDiv.appendChild(SelectionIndexDOM)
-    }
+    SelectionDiv.appendChild(SelectionIndexDOM);
+  }
 }
 
-selectionSetup()
+selectionSetup();
