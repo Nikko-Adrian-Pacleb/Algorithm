b0d50b42379ee9030252c51d4d49be2cf2c346f8
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-04-05 19:11:17 -0700

    Drawing Files and Code Update

--- Visuals/D3Visuals/scripts/Pixel.js
+++ Visuals/D3Visuals/scripts/Pixel.js
@@ -1,6 +1,6 @@
-class CreatePixel {
+class Pixel {
   constructor(xPosition, yPosition) {
-    const pixel = d3
+    this.pixel = d3
       .select("#svg")
       .append("rect")
       .attr("x", xPosition)
@@ -8,12 +8,19 @@ class CreatePixel {
       .attr("width", 10)
       .attr("height", 10)
       .attr("fill", "black")
+      .on("click", function () {
+        d3.select(this).attr("fill", "white");
+      })
       .on("mouseover", function (event, d) {
         // Checks if mouse is down
         if (mouseDown) {
           // Change the color of the pixel to apply draw
-          d3.select(this).style("fill", "white");
+          d3.select(this).attr("fill", "white");
         }
       });
+  }
+
+  resetColor() {
+    this.pixel.attr("fill", "black");
   }
 }
--- Visuals/D3Visuals/scripts/index.js
+++ Visuals/D3Visuals/scripts/index.js
@@ -2,12 +2,12 @@ const width = 900;
 const height = 600;
 
 const svg = d3.select("#svg");
-
+const pixelList = [];
 // Create pixel elements with 10 height and 10 width
 let cursorX = 0;
 let cursorY = 0;
 while (cursorX < width && cursorY < height) {
-  new CreatePixel(cursorX, cursorY);
+  pixelList.push(new Pixel(cursorX, cursorY));
   cursorX += 10;
   if (cursorX == width) {
     cursorY += 10;
@@ -23,3 +23,10 @@ window.onmousedown = () => {
 window.onmouseup = () => {
   mouseDown = false;
 };
+
+// Reset Canvas
+function reset() {
+  for (let i = 0; i < pixelList.length; ++i) {
+    pixelList[i].resetColor();
+  }
+}
